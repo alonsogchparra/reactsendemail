067c96728fa4e72fa93ab38b98e0041053bdb5cd
Alonso Parra <alonsogparra@gmail.com>
2019-03-16 11:03:47 -0400

    Index.js update to nodemailer configuration

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/form', (req, resp) => {
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+      <h3>Contact Detail</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `
+
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'johnpaul.gutkowski@ethereal.email',
+        pass: 'tCwGEN3rEjV3X3k4dE'
+      }
+    });
+
+    let mailOptions = {
+      from: 'test@testaccount.com',
+      to: 'johnpaul.gutkowski@ethereal.email',
+      replyTo: 'test@testaccount.com',
+      subject: 'New Message',
+      text: req.body.message,
+      html: htmlEmail
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log('Message sent: %s', info.message);
+      console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
+    })
+  })
+});
+
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
